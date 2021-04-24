c4d806e371defe6518bf8a9a0b7beb1fae212e4d
ETdoFresh <ETdoFresh@gmail.com>
2021-04-23 23:13:03 -0500

    Add flip on direction.

--- Assets/Scripts/FlipScaleWithDirection.cs
+++ Assets/Scripts/FlipScaleWithDirection.cs
@@ -1,7 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FlipScaleWithDirection : MonoBehaviour
 {
+    private Direction _direction;
+
+    private void Awake()
+    {
+        _direction = transform.parent.GetComponentInChildren<Direction>();
+    }
+
+    private void Update()
+    {
+        if (_direction.IsLeft) FlipLeft();
+        else if (_direction.IsRight) FlipRight();
+    }
+
+    private void FlipLeft()
+    {
+        var scale = transform.localScale;
+        scale.x = -Mathf.Abs(scale.x);
+        transform.localScale = scale;
+    }
+    
+    private void FlipRight()
+    {
+        var scale = transform.localScale;
+        scale.x = Mathf.Abs(scale.x);
+        transform.localScale = scale;
+    }
 }
