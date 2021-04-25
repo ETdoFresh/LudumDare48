13f8f5a8c82c3c62135b856ffa598e9fa6b28cbd
ETdoFresh <ETdoFresh@gmail.com>
2021-04-25 13:17:21 -0500

    Add item random placement. Close #37.

--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
-public class EnemySpawner : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject enemyPrefab;
+    [FormerlySerializedAs("enemyPrefab")] [SerializeField] private GameObject prefab;
     [SerializeField] private GameObject digPrefab;
     [SerializeField] private int enemyCount = 1;
     [SerializeField] private Vector2 minHoleSize = Vector2.one;
     [SerializeField] private Vector2 maxHoleSize = Vector2.one * 5;
+    [SerializeField] private bool digHole;
+    [SerializeField] private string parentName = "---- Characters ---------------------------";
 
     private float Left => transform.position.x - transform.lossyScale.x / 2;
     private float Right => transform.position.x + transform.lossyScale.x / 2;
@@ -24,7 +27,7 @@ public class EnemySpawner : MonoBehaviour
 
     private IEnumerator SpawnEnemy()
     {
-        var enemyParent = GameObject.Find("---- Characters ---------------------------");
+        var parent = GameObject.Find(parentName);
         for (var i = 0; i < enemyCount; i++)
         {
             var randomX = Random.Range(Left, Right);
@@ -35,15 +38,16 @@ public class EnemySpawner : MonoBehaviour
             randomY = Random.Range(minHoleSize.y, maxHoleSize.y);
             var randomScale = new Vector3(randomX, randomY, 1);
 
+            var instance = Instantiate(prefab);
+            instance.name = prefab.name;
+            instance.transform.position = randomPosition;
+            instance.transform.SetParent(parent.transform);
+
+            if (!digHole) continue;
             var dig = Instantiate(digPrefab);
             dig.transform.position = randomPosition;
             dig.transform.localScale = randomScale;
 
-            var enemy = Instantiate(enemyPrefab);
-            enemy.name = enemyPrefab.name;
-            enemy.transform.position = randomPosition;
-            enemy.transform.SetParent(enemyParent.transform);
-            
             yield return new WaitForSeconds(0.5f);
             Destroy(dig);
         }
