1638a5399a1aafd0628bcf603e285e2fd91b67bd
ETdoFresh <ETdoFresh@gmail.com>
2021-04-25 14:15:28 -0500

    Add AreaTextUI.cs. Close #36

---
+++ Assets/Scripts/UI/AreaTextUI.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AreaTextUI : MonoBehaviour
+{
+    [SerializeField] private Player player;
+    [SerializeField] private int currentDepthLevel = 0;
+    [SerializeField] private TextMeshProUGUI textMesh;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private List<DepthText> _depthTexts = new List<DepthText>();
+
+    private float CurrentDepth => player ? player.maxDepth ? player.maxDepth.Value : 0 : 0;
+
+    private void Update()
+    {
+        if (currentDepthLevel >= _depthTexts.Count) return;
+        if (CurrentDepth < _depthTexts[currentDepthLevel].depth) return;
+        textMesh.text = _depthTexts[currentDepthLevel].text;
+        _animator.Play("Fade");
+        currentDepthLevel++;
+    }
+
+    [Serializable]
+    public class DepthText
+    {
+        public float depth;
+        public string text;
+    }
+}
