717b551216b5f9dcaa4e78dee36d5e8a9b44ba7f
ETdoFresh <ETdoFresh@gmail.com>
2021-04-25 20:32:53 -0500

    Final Commit! Close it all out! Close #39. Close #40. Close #41.

--- Assets/Scripts/UI/LoadHighScore.cs
+++ Assets/Scripts/UI/LoadHighScore.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class LoadHighScore : MonoBehaviour
 {
     [SerializeField] private HighScoreList _highScoreList;
+    [SerializeField] private Score score;
 
     private void OnEnable()
     {
+        score.depth = 0;
+        score.time = 0;
         _highScoreList.UpdateScores(this);
     }
 }
